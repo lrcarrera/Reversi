d05eb1b0d9964e755ebf517f7ec5ca2db39a2f2e
lrcarrera <drewico@gmail.com>
2018-05-02 12:21:44 +0200

    imageButton implemented

--- app/src/main/java/com/example/radu/reversi/CustomAdapter.java
+++ app/src/main/java/com/example/radu/reversi/CustomAdapter.java
@@ -12,15 +12,24 @@ import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CustomAdapter extends BaseAdapter {
     private final Context context;
     private Game game;
+    private int size;
     private int number = 1;
+    int BLOCK = 0;
+    int WIN = 1;
+    int LOSE = 2;
+    int DRAW = 3;
+    int TIMER = 4;
 
     public CustomAdapter (Context c, Game game){
         this.context = c;
         this.game = game;
+        this.size = game.getBoard().size();
     }
 
     @Override
@@ -43,22 +52,8 @@ public class CustomAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        convertView = inflater.inflate(R.layout.row, null);
-
-        ImageView cell = convertView.findViewById(R.id.imaginacion);
-
-
-
-
-       /* if (game.getBoard().size() == 6) {
-            ///cell.setLa
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(60, 60);
-            cell.setLayoutParams(layoutParams);
-        } else if (game.getBoard().size() == 4) {
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(80, 80);
-            cell.setLayoutParams(layoutParams);
-        }*/
-        /*ImageButton cell;
+        ImageButton cell;
+        int i,j;
 
         if (convertView == null) {
             cell = new ImageButton(context);
@@ -68,22 +63,10 @@ public class CustomAdapter extends BaseAdapter {
             cell.setPadding(0, 0, 0, 0);
         } else {
             cell = (ImageButton) convertView;
-        }*/
-       /*ImageButton cell;
+        }
 
-        if (convertView == null) {
-            cell = new ImageButton(context);
-            cell.setLayoutParams(new GridView.LayoutParams(parent.getWidth() / game.getBoard().size(), parent.getWidth() /game.getBoard().size()));
-            cell.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            cell.setScaleType(ImageButton.ScaleType.FIT_XY);
-            cell.setPadding(0, 0, 0, 0);
-        } else {
-            //cell  = convertView.findViewById(R.id.imaginacion);
-            cell = (ImageButton) convertView.findViewById(R.id.imaginacion);;
-        }*/
-
-        int i = position % game.getBoard().size();
-        int j = position / game.getBoard().size();
+        i = position % size;
+        j = position / size;
 
 
 
@@ -94,17 +77,155 @@ public class CustomAdapter extends BaseAdapter {
         } else if (game.getBoard().isEmpty(new Position(i,j))){
             cell.setImageResource(R.drawable.green);
         } else if (game.getBoard().isObjective(new Position(i,j))){
-            /*if (number == 1){
-                cell.setImageResource(R.drawable.number1);
-            } else {
-                cell.setImageResource(R.drawable.green);
-            }*/
             cell.setImageResource(R.drawable.whitegreen);
 
         }
 
-        //cell.setOnClickListener(new CellListener(context, new Position(x, y), this.board[x][y], juego));
+        cell.setTag(position);
+
+        cell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int pos = (int) v.getTag();
+                int i = pos % size;
+                int j = pos / size;
+
+
+                if(game.getBoard().isObjective(new Position(i,j))){
+
+
+                    // System.out.println("CUANTASNEGRAS: "+ game.getBoard().getCountBlack());
+                    if(game.getState()==State.BLACK){
+                        System.out.println("ENTRO EN EL BLACK");
+                        game.move(new Position(i,j));
+                        game.setObjectives(size);
+                        game.getBoard().countAll(size);
+                    }
+
+                    //MARCA OBJETIVOS PARA LA MAQUINA
+                    /*game.setObjectives(grid_dimension);
+                    // System.out.println("CUANTASNEGRAS: "+ game.getBoard().getCountBlack());
+
+                    game.getBoard().countAll(grid_dimension);*/
+
+                    //CustomAdapter gnew = (CustomAdapter) parent.getAdapter();
+                    notifyDataSetChanged();
+                    //gv.setAdapter(gnew);
+
+                    //adapter.UpdateGame(game);
+                    //gv.setAdapter(adapter);
+                    //TURNO MAKINA
+                    if(game.getState()==State.WHITE){
+                        System.out.println("ENTRO O KELOKE phone turn");
+                        game.phoneTurn();
+                        //MARCA OBJETIVOS PARA EL PLAYER
+                        game.setObjectives(size);
+
+                        notifyDataSetChanged();
+                       // gnew.notifyDataSetChanged();
+                        //gv.setAdapter(gnew);
+
+                    }
+                    System.out.println("Salio del turno makina");
+                    //game.comprove();
+
+                    if(game.getState() == State.FINISHED){
+                        System.out.println("Entro en la condicion final");
+                        game.getBoard().countAll(size);
+
+                        if(game.blockFinished()){
+                            makeToast(BLOCK);
+                        } else {
+                            if(game.getBoard().getCountBlack() > game.getBoard().getCountWhite()) {
+                                makeToast(WIN);
+                            } else if (game.getBoard().getCountBlack() < game.getBoard().getCountWhite()) {
+                                makeToast(LOSE);
+                            } else {
+                                makeToast(DRAW);
+                            }
+                        }
+
+                        /*Controlar tiempo*/
+                        /*if(){
+                             Toast.makeText(DesarrolloJuego.this, R.string.final_tiempo, Toast.LENGTH_SHORT).show();
+                        }*/
+
+
+                    }
+
+
+
+
+/*
+                    if(game.getBoard().isObjective(new Position(0, 1))){
+                        System.out.println("objetivo MECAGOENTODO\n\n\n");
+
+                    }else{
+                        System.out.println("ALFINNNN");
+                    }
+*/
+
+
+                    // System.out.println("Llego y salio del phonwTurn");
+
+                   /* for (int x = 0; x < grid_dimension; x++){
+                        for (int y = 0; y < grid_dimension; y++){
+                            game.getBoard().getCountBlack();
+                            game.getBoard().getCountWhite();
+                        }
+                    })*/
+
+                    //adapter.notifyDataSetChanged();
+
+                    //adapter.UpdateGame(game);
+
+                    //gv.setAdapter(adapter);
+                    //updateAdapter();
+                } else {
+                    /*No es una casilla valida, sonido incorrecto*/
+                    System.out.println("Casilla incorrecta puslada");
+                }
+            }
+        });
         return cell;
+    }
+    public void makeToast(int i){
+
+        Toast imageToast = new Toast(context);
+        LinearLayout toastLayout = new LinearLayout(context);
+        toastLayout.setOrientation(LinearLayout.HORIZONTAL);
+        ImageView image = new ImageView(context);
+        TextView text = new TextView(context);
+
+        if (i == BLOCK){
+            image.setImageResource(R.drawable.block_icon);
+            text.setText(R.string.bloqueo);
+        } else if (i == WIN){
+            //MediaPlayer ring= MediaPlayer.create(DesarrolloJuego.this, R.raw.win_sound);
+            //ring.start();
+            //System.out.println("Salio del ring");
+            image.setImageResource(R.drawable.like_icon);
+            text.setText(R.string.victoria);
+        } else if (i == LOSE){
+            //MediaPlayer ring= MediaPlayer.create(DesarrolloJuego.this, R.raw.win_sound);
+            //ring.start();
+            image.setImageResource(R.drawable.dislike_icon);
+            text.setText(R.string.perdida);
+        } else if (i == DRAW){
+            image.setImageResource(R.drawable.balance_icon);
+            text.setText(R.string.empate);
+        } else if (i == TIMER){
+            image.setImageResource(R.drawable.chrono_icon);
+            text.setText(R.string.final_tiempo);
+        }
+        toastLayout.addView(image);
+        toastLayout.addView(text);
+        imageToast.setView(toastLayout);
+        imageToast.setDuration(Toast.LENGTH_SHORT);
+        imageToast.show();
+
+
     }
 /*
     public void UpdateGame(Game game) {
--- app/src/main/java/com/example/radu/reversi/DesarrolloJuego.java
+++ app/src/main/java/com/example/radu/reversi/DesarrolloJuego.java
@@ -21,16 +21,8 @@ public class DesarrolloJuego extends AppCompatActivity {
     Board board;
     Game game;
     State state;
-
     GridView gv;
     CustomAdapter adapter;
-
-    int BLOCK = 0;
-    int WIN = 1;
-    int LOSE = 2;
-    int DRAW = 3;
-    int TIMER = 4;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,192 +38,13 @@ public class DesarrolloJuego extends AppCompatActivity {
         gv  = (GridView) findViewById(R.id.grid_custom);
         gv.setNumColumns(grid_dimension);
 
-
         board = new Board(grid_dimension);
         game = new Game(board);
         state = State.BLACK;
 
-
         adapter =  new CustomAdapter(this, game);
-
-
         gv.setAdapter(adapter);
 
-
-        /*for (int x = 0; x < grid_dimension; x++) {
-            for (int y = 0; y < grid_dimension; y++) {
-                if (game.getBoard().isObjective(new Position(x, y))) {
-                    count += 1;
-
-                }
-            }
-        }
-        System.out.println("HAY "+count+" OBJECTIVOS");
-        count=0;*/
-
-
-
-        //    imageButton.setOnClickListener(new CelaListener(context,new Position(x,y),this.board[x][y], joc));
-
-        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                int i = position % game.getBoard().size();
-                int j = position / game.getBoard().size();
-
-
-
-
-                if(game.getBoard().isObjective(new Position(i,j))){
-
-
-                   // System.out.println("CUANTASNEGRAS: "+ game.getBoard().getCountBlack());
-                    if(game.getState()==State.BLACK){
-                        System.out.println("ENTRO EN EL BLACK");
-                        game.move(new Position(i,j));
-                        game.setObjectives(grid_dimension);
-                        game.getBoard().countAll(grid_dimension);
-                    }
-
-                    //MARCA OBJETIVOS PARA LA MAQUINA
-                    /*game.setObjectives(grid_dimension);
-                    // System.out.println("CUANTASNEGRAS: "+ game.getBoard().getCountBlack());
-
-                    game.getBoard().countAll(grid_dimension);*/
-
-                    CustomAdapter gnew = (CustomAdapter) parent.getAdapter();
-                    gnew.notifyDataSetChanged();
-                    gv.setAdapter(gnew);
-
-                    //adapter.UpdateGame(game);
-                    //gv.setAdapter(adapter);
-                    //TURNO MAKINA
-                    if(game.getState()==State.WHITE){
-                        System.out.println("ENTRO O KELOKE phone turn");
-                        game.phoneTurn();
-                        //MARCA OBJETIVOS PARA EL PLAYER
-                        game.setObjectives(grid_dimension);
-
-                        gnew.notifyDataSetChanged();
-                        gv.setAdapter(gnew);
-
-                    }
-                    System.out.println("Salio del turno makina");
-                    //game.comprove();
-
-                    if(game.getState() == State.FINISHED){
-                        System.out.println("Entro en la condicion final");
-                        game.getBoard().countAll(grid_dimension);
-
-                        if(game.blockFinished()){
-                            makeToast(BLOCK);
-                        } else {
-                            if(game.getBoard().getCountBlack() > game.getBoard().getCountWhite()) {
-                                makeToast(WIN);
-                            } else if (game.getBoard().getCountBlack() < game.getBoard().getCountWhite()) {
-                                makeToast(LOSE);
-                            } else {
-                                makeToast(DRAW);
-                            }
-                        }
-
-                        /*Controlar tiempo*/
-                        /*if(){
-                             Toast.makeText(DesarrolloJuego.this, R.string.final_tiempo, Toast.LENGTH_SHORT).show();
-                        }*/
-
-
-                    }
-
-
-
-
-/*
-                    if(game.getBoard().isObjective(new Position(0, 1))){
-                        System.out.println("objetivo MECAGOENTODO\n\n\n");
-
-                    }else{
-                        System.out.println("ALFINNNN");
-                    }
-*/
-
-
-                   // System.out.println("Llego y salio del phonwTurn");
-
-                   /* for (int x = 0; x < grid_dimension; x++){
-                        for (int y = 0; y < grid_dimension; y++){
-                            game.getBoard().getCountBlack();
-                            game.getBoard().getCountWhite();
-                        }
-                    })*/
-
-                    //adapter.notifyDataSetChanged();
-
-                    //adapter.UpdateGame(game);
-
-                    //gv.setAdapter(adapter);
-                    //updateAdapter();
-                } else {
-                    /*No es una casilla valida, sonido incorrecto*/
-                    System.out.println("Casilla incorrecta puslada");
-                }
-
-
-                //gv.setAdapter(adapter);
-                //adapter.notifyDataSetChanged();
-                //gv.setAdapter(adapter);
-                //adapter.getItem(position).setLiked(True);
-                //adapter.notifyDataSetChanged();
-                //gv.setAdapter(adapter);
-
-
-            }
-        });
-
-
     }
 
-    private void updateAdapter(){
-        Toast.makeText(this,String.valueOf(game.getBoard().getCountBlack()), Toast.LENGTH_LONG).show();
-
-    }
-
-    public void makeToast(int i){
-
-        Toast imageToast = new Toast(getBaseContext());
-        LinearLayout toastLayout = new LinearLayout(getBaseContext());
-        toastLayout.setOrientation(LinearLayout.HORIZONTAL);
-        ImageView image = new ImageView(getBaseContext());
-        TextView text = new TextView(getBaseContext());
-
-        if (i == BLOCK){
-            image.setImageResource(R.drawable.block_icon);
-            text.setText(R.string.bloqueo);
-        } else if (i == WIN){
-            //MediaPlayer ring= MediaPlayer.create(DesarrolloJuego.this, R.raw.win_sound);
-            //ring.start();
-            //System.out.println("Salio del ring");
-            image.setImageResource(R.drawable.like_icon);
-            text.setText(R.string.victoria);
-        } else if (i == LOSE){
-            //MediaPlayer ring= MediaPlayer.create(DesarrolloJuego.this, R.raw.win_sound);
-            //ring.start();
-            image.setImageResource(R.drawable.dislike_icon);
-            text.setText(R.string.perdida);
-        } else if (i == DRAW){
-            image.setImageResource(R.drawable.balance_icon);
-            text.setText(R.string.empate);
-        } else if (i == TIMER){
-            image.setImageResource(R.drawable.chrono_icon);
-            text.setText(R.string.final_tiempo);
-        }
-        toastLayout.addView(image);
-        toastLayout.addView(text);
-        imageToast.setView(toastLayout);
-        imageToast.setDuration(Toast.LENGTH_SHORT);
-        imageToast.show();
-
-
-    }
 }
